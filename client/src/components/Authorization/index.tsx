707689807a555ec826422644beea7860ba1aef16
unknown <bydlocoder228@gmail.com>
2019-11-24 21:52:13 +0200

    a lil bit refactored

--- client/src/components/Authorization/index.tsx
+++ client/src/components/Authorization/index.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import gql from 'graphql-tag';
-import { useMutation } from '@apollo/react-hooks';
 
 import HeaderContext from '../Header/HeaderContext';
 import Login from './Login';
