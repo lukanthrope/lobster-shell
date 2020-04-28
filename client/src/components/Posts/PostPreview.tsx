85b766e171d0ebd0853561ee1f2859fc205d9aa8
unknown <bydlocoder228@gmail.com>
2020-04-29 02:31:56 +0300

    responsive grid

--- client/src/components/Posts/PostPreview.tsx
+++ client/src/components/Posts/PostPreview.tsx
@@ -19,19 +19,25 @@ const PostPreview = ({
   distance, 
   location, 
   id }: PostProps) => 
-  <div className="f-basis(33%) w(300px) w-child(300px) h(300px)">
-      { isTaken && <p>(in use now)</p> }
+  <div className="f-basis(33%) m-b(70px)">
+    <div className="pos(r) m(auto) w(300px) h(300px) w-child(300px) shad">
       <Link className="text-dec(none)" to={id}>
-        <img className="w(300px) image-land" src={picture || defaultImage} />  
-        <h3 className="color(lobster-pink)">{title}</h3>    
+        <img className={`w(300px) h(200px) image-land ${isTaken ? 'opacity(0.5)' : ''}`} src={picture || defaultImage} />   
       </Link>
-      <span className="d(in-flex)">
-        <i className="material-icons errorMes">
-            room    
-        </i>
-        <p>{location} </p>
-      </span>
-      { distance && <p> ~{distance} away</p> }
+      <div className="pos(a) bottom(1px) left(0) right(0) m-l(auto) m-r(auto)">
+        <Link className="text-dec(none)" to={id}> 
+          <h3 className="m-t(30px) color(lobster-pink)">{title}</h3>    
+        </Link>
+        <span className="d(in-flex)">
+          <i className="material-icons errorMes">
+              room    
+          </i>
+          <p>{location}</p>
+        </span>
+      </div>
+      { isTaken && <p className="pos(a) fs(1.3rem) fs(600) errorMes top(10px) right(10px)">in use now</p> }
+      { distance && <p className="pos(r) color(gray) m-t(5px) left(-20px)">{distance} away</p> }
+    </div>
   </div>
 
 export default PostPreview;
