3c7f5d751669c2abeb683299b65c901a59460435
lukanthrope <bydlocoder228@gmail.com>
2021-11-26 01:53:33 +0200

    remove

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB: 'mongodb+srv://andrew:100500and@cluster0-1arci.mongodb.net/test?retryWrites=true&w=majority',
+  MONGODB: '',
   SECRET_KEY: 'some very secret key',
 }
